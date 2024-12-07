0b45a18273c6845b547ae726a158edf4e249fb52
dktsitun <esitun@direkto.com.gt>
2024-12-07 10:33:52 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./styles.scss";
+
+document.getElementById("app").innerHTML = `
+<div class="card">
+  <div class="back"></div>
+  <div class="front"></div> <!-- Imagen de fondo aplicada con CSS -->
+
+  <div class="text-container">
+    
+
+  
+  <div class="map-link">
+    <a href="https://maps.app.goo.gl/5H4xKDYjP654YsST6" target="_blank" class="link highlight">(click 🚗 Waze o Maps)</a>
+  </div>
+
+
+  </div>
+</div>
+`;
